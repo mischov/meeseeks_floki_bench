698b1a45184cb45c9789a822e44fe52ea6d0e749
Mischov <mmischov@gmail.com>
2020-04-29 12:10:08 -0500

    Update benchmark code as appropriate for latest versions of Meeseeks and Floki

--- lib/meeseeks_floki_bench/trending_js.ex
+++ lib/meeseeks_floki_bench/trending_js.ex
@@ -9,7 +9,10 @@ defmodule MeeseeksFlokiBench.TrendingJs do
   # Floki
 
   def floki_trending_js_css(html) do
-    Floki.find(html, "ol.repo-list > li")
+    {:ok, document} = Floki.parse_document(html)
+
+    document
+    |> Floki.find("ol.repo-list > li")
     |> Enum.map(fn result ->
       %{
         name: floki_repo_name_css(result),
@@ -20,21 +23,24 @@ defmodule MeeseeksFlokiBench.TrendingJs do
   end
 
   defp floki_repo_name_css(result) do
-    Floki.find(result, "div h3 a")
+    result
+    |> Floki.find("div h3 a")
     |> List.first()
     |> Floki.text(deep: false)
     |> String.trim()
   end
 
   defp floki_repo_stars_css(result) do
-    Floki.find(result, "div:nth-child(4) > :nth-child(3)")
+    result
+    |> Floki.find("div:nth-child(4) > :nth-child(3)")
     |> List.first()
     |> Floki.text(deep: false)
     |> String.trim()
   end
 
   defp floki_repo_stars_today_css(result) do
-    Floki.find(result, "div:nth-child(4) > :nth-child(6)")
+    result
+    |> Floki.find("div:nth-child(4) > :nth-child(6)")
     |> List.first()
     |> Floki.text(deep: false)
     |> String.trim()
@@ -43,7 +49,9 @@ defmodule MeeseeksFlokiBench.TrendingJs do
   # Meeseeks CSS
 
   def meeseeks_trending_js_css(html) do
-    Meeseeks.all(html, css("ol.repo-list > li"))
+    html
+    |> Meeseeks.parse(:html)
+    |> Meeseeks.all(css("ol.repo-list > li"))
     |> Enum.map(fn result ->
       %{
         name: meeseeks_repo_name_css(result),
@@ -54,25 +62,31 @@ defmodule MeeseeksFlokiBench.TrendingJs do
   end
 
   defp meeseeks_repo_name_css(result) do
-    Meeseeks.one(result, css("div h3 a"))
-    # Already trims text, so no trim
-    |> Meeseeks.own_text()
+    result
+    |> Meeseeks.one(css("div h3 a"))
+    # Meeseeks.own_text trims by default, but turn off whitespace_collapse
+    # for equal comparison with Floki.text
+    |> Meeseeks.own_text(collapse_whitespace: false)
   end
 
   defp meeseeks_repo_stars_css(result) do
-    Meeseeks.one(result, css("div:nth-child(4) > :nth-child(3)"))
-    |> Meeseeks.own_text()
+    result
+    |> Meeseeks.one(css("div:nth-child(4) > :nth-child(3)"))
+    |> Meeseeks.own_text(collapse_whitespace: false)
   end
 
   defp meeseeks_repo_stars_today_css(result) do
-    Meeseeks.one(result, css("div:nth-child(4) > :nth-child(6)"))
-    |> Meeseeks.own_text()
+    result
+    |> Meeseeks.one(css("div:nth-child(4) > :nth-child(6)"))
+    |> Meeseeks.own_text(collapse_whitespace: false)
   end
 
   # Meeseeks XPath
 
   def meeseeks_trending_js_xpath(html) do
-    Meeseeks.all(html, xpath("ol[contains(@class, 'repo-list')]/li"))
+    html
+    |> Meeseeks.parse(:html)
+    |> Meeseeks.all(xpath("ol[contains(@class, 'repo-list')]/li"))
     |> Enum.map(fn result ->
       %{
         name: meeseeks_repo_name_xpath(result),
@@ -83,18 +97,22 @@ defmodule MeeseeksFlokiBench.TrendingJs do
   end
 
   defp meeseeks_repo_name_xpath(result) do
-    Meeseeks.one(result, xpath("./div/h3/a"))
-    # Already trims text, so no trim
-    |> Meeseeks.own_text()
+    result
+    |> Meeseeks.one(xpath("./div/h3/a"))
+    # Meeseeks.own_text trims by default, but turn off whitespace_collapse
+    # for equal comparison with Floki.text
+    |> Meeseeks.own_text(collapse_whitespace: false)
   end
 
   defp meeseeks_repo_stars_xpath(result) do
-    Meeseeks.one(result, xpath("./div[4]/*[3]"))
-    |> Meeseeks.own_text()
+    result
+    |> Meeseeks.one(xpath("./div[4]/*[3]"))
+    |> Meeseeks.own_text(collapse_whitespace: false)
   end
 
   defp meeseeks_repo_stars_today_xpath(result) do
-    Meeseeks.one(result, xpath("./div[4]/*[6]"))
-    |> Meeseeks.own_text()
+    result
+    |> Meeseeks.one(xpath("./div[4]/*[6]"))
+    |> Meeseeks.own_text(collapse_whitespace: false)
   end
 end
--- lib/meeseeks_floki_bench/wiki_links.ex
+++ lib/meeseeks_floki_bench/wiki_links.ex
@@ -10,31 +10,37 @@ defmodule MeeseeksFlokiBench.WikiLinks do
   # Floki
 
   def floki_wiki_links_css(html) do
-    Floki.find(html, "#mw-content-text > p a[href^=\"/wiki/\"]")
+    {:ok, document} = Floki.parse_document(html)
+
+    document
+    |> Floki.find("#mw-content-text > p a[href^=\"/wiki/\"]")
     |> Floki.attribute("href")
   end
 
   # Meeseeks CSS
 
   def meeseeks_wiki_links_css(html) do
-    Meeseeks.all(html, css("#mw-content-text > p a[href^=\"/wiki/\"]"))
+    html
+    |> Meeseeks.parse(:html)
+    |> Meeseeks.all(css("#mw-content-text > p a[href^=\"/wiki/\"]"))
     |> Enum.map(&Meeseeks.attr(&1, "href"))
   end
 
   # Meeseeks XPath Naive
 
   def meeseeks_wiki_links_xpath_naive(html) do
-    Meeseeks.all(html, xpath("div[@id='mw-content-text']/p//a[starts-with(@href, '/wiki/')]"))
+    html
+    |> Meeseeks.parse(:html)
+    |> Meeseeks.all(xpath("div[@id='mw-content-text']/p//a[starts-with(@href, '/wiki/')]"))
     |> Enum.map(&Meeseeks.attr(&1, "href"))
   end
 
   # Meeseeks XPath Optimized
 
   def meeseeks_wiki_links_xpath_optimized(html) do
-    Meeseeks.all(
-      html,
-      xpath("/html/body/div[3]/div[3]/div[4]/p//a[starts-with(@href, '/wiki/')]")
-    )
+    html
+    |> Meeseeks.parse(:html)
+    |> Meeseeks.all(xpath("/html/body/div[3]/div[3]/div[4]/p//a[starts-with(@href, '/wiki/')]"))
     |> Enum.map(&Meeseeks.attr(&1, "href"))
   end
 end
